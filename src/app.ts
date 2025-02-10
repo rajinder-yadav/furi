4a048d142f2f8e3005e150d789a807c46932a22e
Dev Guy <devguy.ca@gmail.com>
2025-02-10 03:39:27 -0500

    refector: callback switch over to application context

--- src/app.ts
+++ src/app.ts
@@ -10,139 +10,142 @@
 
 // deno-lint-ignore-file no-unused-vars
 
-import { Furi, HttpRequest, HttpResponse } from "./furi.ts";
+import { Furi, ApplicationContext } from "./furi.ts";
 import { Buffer } from 'node:buffer';
 
 const furi = Furi.create();
 const USER_AGENT: string = "FURI Node Server (v0.1)";
 
-furi.get("/", (req: HttpRequest, res: HttpResponse) => {
+furi.saveStoreData("db", { database: "Sqlite3" });
 
-  res.writeHead(200, {
+furi.get("/", (ctx: ApplicationContext) => {
+
+
+  ctx.response.writeHead(200, {
     "Content-Type": "text/html",
     "User-Agent": USER_AGENT
   });
 
-  res.write("<h1>FURI</h1>\n");
-  res.write("<p>Welcome to Node FURI, the fast and furiour Node Router!</p>\n");
-  res.end();
+  ctx.response.write("<h1>FURI</h1>\n");
+  ctx.response.write("<p>Welcome to Node FURI, the fast and furiour Node Router!</p>\n");
+  ctx.response.end();
 
 });
 
-furi.get("/about", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/about", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/html",
     "User-Agent": USER_AGENT
   });
 
-  res.end("<h1>About FURI</h1>\nThis is the about page.\n");
+  ctx.response.end("<h1>About FURI</h1>\nThis is the about page.\n");
 
 });
 
-furi.get("/about/:user_id", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/about/:user_id", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/html",
     "User-Agent": USER_AGENT
   });
 
-  res.end(`<h1>About User Page!</h1>\nUser page for: ${req.params.user_id}\n`);
+  ctx.response.end(`<h1>About User Page!</h1>\nUser page for: ${ctx.request.params.user_id}\n`);
 
 });
 
-furi.get("/user/:user_id/photo/:photo_id", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/user/:user_id/photo/:photo_id", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/html",
     "User-Agent": USER_AGENT
   });
 
-  res.end(`<h1>User Photo Page!</h1>\nUser ${req.params.user_id} photo ${req.params.photo_id}\n`);
+  ctx.response.end(`<h1>User Photo Page!</h1>\nUser ${ctx.request.params.user_id} photo ${ctx.request.params.photo_id}\n`);
 
 });
 
 
-furi.get("/tor+onto/:code/ca\\d*n$", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/tor+onto/:code/ca\\d*n$", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/html",
     "User-Agent": USER_AGENT
   });
 
-  res.end(`<h1>Toronto Canada</h1>\nCode is ${req.params.code}\n`);
+  ctx.response.end(`<h1>Toronto Canada</h1>\nCode is ${ctx.request.params.code}\n`);
 
 });
 
 // Handlers can be chained
 // return true to terminate the call chain at any point.
-furi.get("/chain", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/chain", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/html",
     "User-Agent": USER_AGENT
   });
 
-  res.write("<h1>Chained Handlers</h1>\n<p>This paragraph is form handler 1</p>\n");
+  ctx.response.write("<h1>Chained Handlers</h1>\n<p>This paragraph is form handler 1</p>\n");
   // Uncomment next line to stop here!
   // return true;
 
-}, (req: HttpRequest, res: HttpResponse) => {
+}, (ctx: ApplicationContext) => {
 
-  res.end("<p>This paragraph is form handler 2</p>\n");
+  ctx.response.end("<p>This paragraph is form handler 2</p>\n");
 
 });
 
 // Handlers can be chained
 // return true to terminate the call chain at any point.
-furi.get("/chainhalt", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/chainhalt", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/html",
     "User-Agent": USER_AGENT
   });
 
-  res.write("<h1>Chained Handlers</h1>\n<p>This paragraph is form handler 1</p>\n");
+  ctx.response.write("<h1>Chained Handlers</h1>\n<p>This paragraph is form handler 1</p>\n");
   // Uncomment next 2 lines to stop here!
   return true;
 
-}, (req: HttpRequest, res: HttpResponse) => {
+}, (ctx: ApplicationContext) => {
 
-  res.end("<p>This paragraph is form handler 2</p>\n");
+  ctx.response.end("<p>This paragraph is form handler 2</p>\n");
 
 });
 
 // HTTP PATCH
-furi.patch("/comment", (req: HttpRequest, res: HttpResponse) => {
+furi.patch("/comment", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end("PATCH a fresh comment.");
+  ctx.response.end("PATCH a fresh comment.");
 
 });
 
-furi.patch("/comment/how-to", (req: HttpRequest, res: HttpResponse) => {
+furi.patch("/comment/how-to", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end("PATCH How to post a comment page.");
+  ctx.response.end("PATCH How to post a comment page.");
 
 });
 
-furi.patch("/comment/:id", (req: HttpRequest, res: HttpResponse) => {
+furi.patch("/comment/:id", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "application/json",
     "User-Agent": USER_AGENT
   });
@@ -150,46 +153,46 @@ furi.patch("/comment/:id", (req: HttpRequest, res: HttpResponse) => {
   const body: Uint8Array[] = [];
   let text: string;
 
-  req.on("data", chunk => {
+  ctx.request.on("data", chunk => {
     body.push(chunk);
   }).
     on("end", () => {
       text = Buffer.concat(body).toString();
-      const data = { message: "PATCH comment with id", id: req.params.id, text: text };
-      res.end(JSON.stringify(data));
+      const data = { message: "PATCH comment with id", id: ctx.request.params.id, text: text };
+      ctx.response.end(JSON.stringify(data));
     });
 
 });
 
 // HTTP POST
-furi.post("/comment", (req: HttpRequest, res: HttpResponse) => {
+furi.post("/comment", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end("POST a fresh comment.");
+  ctx.response.end("POST a fresh comment.");
 
 });
 
-furi.post("/comment/how-to", (req: HttpRequest, res: HttpResponse) => {
+furi.post("/comment/how-to", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end("POST How to post a comment page.");
+  ctx.response.end("POST How to post a comment page.");
 
 });
 
-furi.post("/comment/:id", (req: HttpRequest, res: HttpResponse) => {
+furi.post("/comment/:id", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "application/json",
     "User-Agent": USER_AGENT
   });
@@ -197,190 +200,190 @@ furi.post("/comment/:id", (req: HttpRequest, res: HttpResponse) => {
   const body: Uint8Array[] = [];
   let text: string;
 
-  req.on("data", chunk => {
+  ctx.request.on("data", chunk => {
     body.push(chunk);
   }).
     on("end", () => {
       text = Buffer.concat(body).toString();
-      const data = { message: "POST comment with id", id: req.params.id, text: text };
-      res.end(JSON.stringify(data));
+      const data = { message: "POST comment with id", id: ctx.request.params.id, text: text };
+      ctx.response.end(JSON.stringify(data));
 
     });
 
 });
 
 // HTTP PUT
-furi.put("/comment", (req: HttpRequest, res: HttpResponse) => {
+furi.put("/comment", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   })
 
-  res.end("PUT a fresh comment.");
+  ctx.response.end("PUT a fresh comment.");
 
 });
 
-furi.put("/comment/how-to", (req: HttpRequest, res: HttpResponse) => {
+furi.put("/comment/how-to", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end("PUT How to post a comment page.");
+  ctx.response.end("PUT How to post a comment page.");
 
 });
 
-furi.put("/comment/:id", (req: HttpRequest, res: HttpResponse) => {
+furi.put("/comment/:id", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end(`PUT comment with id: ${req.params.id}`);
+  ctx.response.end(`PUT comment with id: ${ctx.request.params.id}`);
 
 });
 
 // HTTP DELETE
-furi.delete("/comment", (req: HttpRequest, res: HttpResponse) => {
+furi.delete("/comment", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end("DELETE a comment.");
+  ctx.response.end("DELETE a comment.");
 
 });
 
-furi.delete("/comment/how-to", (req: HttpRequest, res: HttpResponse) => {
+furi.delete("/comment/how-to", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end("DELETE How to post a comment page.");
+  ctx.response.end("DELETE How to post a comment page.");
 
 });
 
-furi.delete("/comment/:id", (req: HttpRequest, res: HttpResponse) => {
+furi.delete("/comment/:id", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end(`DELETE comment with id: ${req.params.id}`);
+  ctx.response.end(`DELETE comment with id: ${ctx.request.params.id}`);
 
 });
 
-furi.use("/middleware", (req: HttpRequest, res: HttpResponse) => {
-  res.writeHead(200, {
+furi.use("/middleware", (ctx: ApplicationContext) => {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
-  res.write("About page Middleware 1\n");
+  ctx.response.write("About page Middleware 1\n");
 });
 //
-furi.use("/middleware", (req: HttpRequest, res: HttpResponse) => {
-  res.writeHead(200, {
+furi.use("/middleware", (ctx: ApplicationContext) => {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
-  res.write("About page Middleware 2\n");
+  ctx.response.write("About page Middleware 2\n");
   return true;
 });
 
-furi.get("/middleware", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/middleware", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/html",
     "User-Agent": USER_AGENT
   });
 
-  res.write("<h1>About FURI</h1>\nThis is the about page.\n");
-  res.end();
+  ctx.response.write("<h1>About FURI</h1>\nThis is the about page.\n");
+  ctx.response.end();
 
 });
 
 /**
  * The next three test cases are for testing, pre, main, post requests.
- * The chained requires act like a stacked middlewares.
+ * The chained requires act like a stacked middlewactx.response.
  */
-furi.get("/middleware2", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/middleware2", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/html",
     "User-Agent": USER_AGENT
   });
 
-  res.write("<p>Middleware 2 pre</p>\n");
+  ctx.response.write("<p>Middleware 2 pre</p>\n");
 
 });
-furi.get("/middleware2", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/middleware2", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/html",
     "User-Agent": USER_AGENT
   });
 
-  res.write("<p>Middleware 2 GET </p>\n");
+  ctx.response.write("<p>Middleware 2 GET </p>\n");
 
 });
-furi.get("/middleware2", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/middleware2", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/html",
     "User-Agent": USER_AGENT
   });
 
-  res.end("<p>Middleware 2 post </p>\n");
+  ctx.response.end("<p>Middleware 2 post </p>\n");
 
 });
 
-furi.get("/query-check", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/query-check", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "application/json",
     "User-Agent": USER_AGENT
   });
 
-  res.end(JSON.stringify(req.query));
+  ctx.response.end(JSON.stringify(ctx.request.query));
 
 });
 
 // HTTP GET RegEx path
-furi.get("/regex/:id/dept\\d+", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/regex/:id/dept\\d+", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   })
 
-  res.end("RegEx path matched.");
+  ctx.response.end("RegEx path matched.");
 
 });
 
 // HTTP GET RegEx path
-furi.get("/regex/\\d?to*ronto", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/regex/\\d?to*ronto", (ctx: ApplicationContext) => {
 
   // Header can only be set once!
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   })
 
-  res.end("RegEx for path toronto matched.");
+  ctx.response.end("RegEx for path toronto matched.");
 
 });
 
@@ -388,25 +391,25 @@ furi.get("/regex/\\d?to*ronto", (req: HttpRequest, res: HttpResponse) => {
  * This test and the next, are to make sure that
  * Static routes are matched before Named routes.
  */
-furi.get("/foo/:bar", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/foo/:bar", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end(`Foo ${req.params.bar}`);
+  ctx.response.end(`Foo ${ctx.request.params.bar}`);
 
 });
 
-furi.get("/foo/bar", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/foo/bar", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end("Foo Bar");
+  ctx.response.end("Foo Bar");
 
 });
 
@@ -415,14 +418,14 @@ furi.get("/foo/bar", (req: HttpRequest, res: HttpResponse) => {
  * This test and the next, are to make sure that
  * Static routes are matched before Named routes.
  */
-furi.get("/foo/:bar/bar1", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/foo/:bar/bar1", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end(`Foo ${req.params.bar} bar1`);
+  ctx.response.end(`Foo ${ctx.request.params.bar} bar1`);
 
 });
 
@@ -430,14 +433,14 @@ furi.get("/foo/:bar/bar1", (req: HttpRequest, res: HttpResponse) => {
  * This test and the next, are to make sure that
  * Static routes are matched before Named routes.
  */
-furi.get("/foo/:bar/bar2/bar", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/foo/:bar/bar2/bar", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end(`Foo ${req.params.bar} bar2 bar`);
+  ctx.response.end(`Foo ${ctx.request.params.bar} bar2 bar`);
 
 });
 
@@ -445,14 +448,14 @@ furi.get("/foo/:bar/bar2/bar", (req: HttpRequest, res: HttpResponse) => {
  * This test and the next, are to make sure that
  * Static routes are matched before Named routes.
  */
-furi.get("/foo/:bar/bar2", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/foo/:bar/bar2", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end(`Foo ${req.params.bar} bar2`);
+  ctx.response.end(`Foo ${ctx.request.params.bar} bar2`);
 
 });
 
@@ -460,14 +463,14 @@ furi.get("/foo/:bar/bar2", (req: HttpRequest, res: HttpResponse) => {
  * This test and the next, are to make sure that
  * Static routes are matched before Named routes.
  */
-furi.get("/foo/:bar/bar2/car", (req: HttpRequest, res: HttpResponse) => {
+furi.get("/foo/:bar/bar2/car", (ctx: ApplicationContext) => {
 
-  res.writeHead(200, {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/plain",
     "User-Agent": USER_AGENT
   });
 
-  res.end(`Foo ${req.params.bar} bar2 car`);
+  ctx.response.end(`Foo ${ctx.request.params.bar} bar2 car`);
 
 });
 
@@ -475,40 +478,42 @@ furi.get("/foo/:bar/bar2/car", (req: HttpRequest, res: HttpResponse) => {
 /**
  * Next 2 test the ALL and GET request handling.
  */
-furi.all('/all', (req: HttpRequest, res: HttpResponse) => {
-  res.writeHead(200, {
+furi.all('/all', (ctx: ApplicationContext) => {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/html",
     "User-Agent": USER_AGENT
   });
 
-  res.write("<h1>All Page</h1>\n");
+  ctx.response.write("<h1>All Page</h1>\n");
 
 });
 
-furi.get('/all', (req: HttpRequest, res: HttpResponse) => {
-  res.writeHead(200, {
+furi.get('/all', (ctx: ApplicationContext) => {
+  ctx.response.writeHead(200, {
     "Content-Type": "text/html",
     "User-Agent": USER_AGENT
   });
-
-  res.end("<p>A paragraph all about nothing.\n");
+  ctx.response.end("<p>A paragraph all about nothing.\n");
 
 });
 
 /**
  * Set middleware request session data.
  */
-furi.use( (req: HttpRequest, res: HttpResponse) => {
+furi.use((ctx: ApplicationContext) => {
+  ctx.app.saveStoreData('msg', { message: 'Store data' });
   console.log('Root Middleware - pre');
-});
-furi.use( (req: HttpRequest, res: HttpResponse) => {
-  if(req.sessionData) {
-    req.sessionData.message = "Root middleware session data";
+  if (ctx.request.sessionData) {
+    ctx.request.sessionData.message = "Root middleware session data";
   }
-  // console.log('Middleware');
 });
-furi.use( (req: HttpRequest, res: HttpResponse) => {
-  console.log(`${req.sessionData?.message as string} - post`);
+furi.use((ctx: ApplicationContext) => {
+  console.log('Root Middleware - main');
+});
+furi.use((ctx: ApplicationContext) => {
+  console.log('Request session data> ', ctx.request.sessionData?.message as string);
+  console.log('Store data for property "MSG"> ', ctx.app.loadStoreData('msg'));
+  console.log('Store data for property "db"> ', ctx.app.loadStoreData('db'));
 });
 
 // const server = http.createServer(furi.handler()).listen(SERVER_PORT, SERVER_HOSTNAME)
