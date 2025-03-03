c577a23bb3d8ec303ee673047622e11e748e50fb
Dev Guy <devguy.ca@gmail.com>
2025-03-03 17:11:49 -0500

    git: moving example to a separate repository

--- furi-examples/01-simple-js-node/main.js
+++
@@ -1,7 +0,0 @@
-import { Furi  } from '@furi-server/furi';
-
-const app = new Furi();
-app.get('/', ctx => {
-  return { message: 'Hello from Furi' };
-});
-app.start();
--- furi-examples/03-simple-ts/main.ts
+++
@@ -1,7 +0,0 @@
-import { Furi, ApplicationContext  } from '@furi-server/furi';
-
-const app = new Furi();
-app.get('/', (ctx: ApplicationContext) => {
-  return { message: 'Hello From Furi' };
-});
-app.start();
