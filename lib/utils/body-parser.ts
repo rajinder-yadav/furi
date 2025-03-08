128df38efee15b9a83515d746ed5d34b37a4f820
Dev Guy <devguy.ca@gmail.com>
2025-03-08 17:36:31 -0500

    edit: debug logs commented out

--- lib/utils/body-parser.ts
+++ lib/utils/body-parser.ts
@@ -10,7 +10,7 @@
 
 import { ApplicationContext } from '../application-context.ts';
 import { Middleware } from "../types.ts";
-import { Furi, LOG_ERROR } from "../furi.ts";
+import { Furi, LOG_DEBUG, LOG_ERROR } from "../furi.ts";
 
 /**
  * Middleware function to parse JSON bodies.
@@ -20,23 +20,32 @@ import { Furi, LOG_ERROR } from "../furi.ts";
  */
 export function JSONBodyParser(ctx: ApplicationContext, next: Middleware): any {
   const body: Uint8Array[] = [];
-
+  // LOG_DEBUG('JSONBodyParser main body enter');
   ctx.request
-    .on("data", (chunk: Uint8Array) => {
-      body.push(chunk);
-    })
-    .on("end", () => {
-      try {
-        if (body.length > 0) {
-          const resultBody = JSON.parse(body.concat().toString());
-          Furi.appStore.storeState('body', resultBody);
-          ctx.request.body = resultBody; // Assign parsed body to request object for easy access
-        }
-        next();
-      } catch (error) {
-        LOG_ERROR(`jsonBodyParser: parsing JSON body: ${error}`);
-        ctx.response.writeHead(400, { 'Content-Type': 'application/json' });
-        ctx.end({ error: 'Invalid JSON' });
+  .on("data", (chunk: Uint8Array) => {
+    // LOG_DEBUG('JSONBodyParser on data');
+    body.push(chunk);
+  })
+  .on("end", () => {
+    // LOG_DEBUG(`JSONBodyParser body[]: ${body.length}`);
+    // LOG_DEBUG(`JSONBodyParser body[]: ${body.concat().toString()}`);
+    try {
+      // LOG_DEBUG('JSONBodyParser on end');
+      if (body.length > 0) {
+        // LOG_DEBUG('JSONBodyParser on JSON parse start');
+        const resultBody = JSON.parse(body.concat().toString());
+        Furi.appStore.storeState('body', resultBody);
+        ctx.request.body = resultBody; // Assign parsed body to request object for easy access
+        // LOG_DEBUG('JSONBodyParser on JSON parse end');
       }
-    });
+      // LOG_DEBUG('JSONBodyParser main next start');
+      next();
+      // LOG_DEBUG('JSONBodyParser main next end');
+    } catch (error) {
+      LOG_ERROR(`jsonBodyParser: parsing JSON body: ${error}`);
+      ctx.response.writeHead(400, { 'Content-Type': 'application/json' });
+      ctx.end({ error: 'Invalid JSON' });
+    }
+  });
+  // LOG_DEBUG('JSONBodyParser main body exit');
 }
